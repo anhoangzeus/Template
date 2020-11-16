ab44a5e070d113253438b3e680b19b362a479d16
Tienanhdeptrai <nguyentienanh1631999@gmail.com>
2020-11-16 19:17:37 +0700

    11/16

--- DemoTem/screens/Order.js
+++ DemoTem/screens/Order.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { fbApp } from '../firebaseconfig';
 
-const Order = () => {
+const Order = ({navigation}) => {
+  function GetListOrder = ()=>{
+
+      fbApp.database().ref("Orders").on('value', snapshot=>{
+        var items = [];
+        snapshot.forEach(function(order_Snapshot){
+          items.push({
+            OrderID:order_Snapshot.val().OrderID,
+            CreatedDate:order_Snapshot.val().CreatedDate,
+            ShipAddress:order_Snapshot.val().ShipAddress,
+            ShipName:order_Snapshot.val().ShipName,
+            ShipMoblie:order_Snapshot.val().ShipMoblie,
+          })
+        }) 
+      })
+  }
+  const [data, setdata]  = useState(initialState);
     return (
       <View style={styles.container}>
         <Text>InfoUser Screen</Text>
